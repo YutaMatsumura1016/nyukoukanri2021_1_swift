e0cb76a0654e3e66ae9a0086e6d12422286b17f7
YutaMatsumura <yuta.matsumura@wasedasai.net>
2021-08-19 13:05:27 +0900

    デザイン以外完成(8/19)

--- nyukoukanri2021_1/ViewController.swift
+++ nyukoukanri2021_1/ViewController.swift
@@ -2,12 +2,57 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController {
-
+    
+    //初期化
+    var gate: String = "str"
+    
+    //ボタンの設定
+    @IBOutlet weak var buttonWaseda: UIButton!
+    @IBOutlet weak var buttonToyama: UIButton!
+    @IBOutlet weak var buttonLogin: UIButton!
+    @IBOutlet weak var buttonFinishLogin: UIButton!
+    @IBOutlet weak var loginWebView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        buttonFinishLogin.isHidden = true
+    }
+    
+    
+    //ボタン押された
+    @IBAction func Waseda(_ sender: Any) {
+        gate = "waseda"
+        performSegue(withIdentifier: "toWaseda", sender: nil)
+    }
+    @IBAction func Toyama(_ sender: Any) {
+        gate = "toyama"
+        performSegue(withIdentifier: "toToyama", sender: nil)
+    }
+    @IBAction func Login(_ sender: Any) {
+        let sentURL = "https://yahoo.co.jp"
+        let encordedURL = sentURL.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)
+        let URL = NSURL(string: encordedURL!)
+        let request = NSURLRequest(url: URL! as URL)
+
+        loginWebView.load(request as URLRequest)
+        buttonFinishLogin.isHidden = false
+        buttonWaseda.isHidden = true
+        buttonToyama.isHidden = true
+        buttonLogin.isHidden = true
+    }
+    @IBAction func closeWebView(_ sender: Any) {
+        loginWebView.isHidden = true
+        buttonFinishLogin.isHidden = true
+        buttonLogin.isHidden = true
+        buttonWaseda.isHidden = false
+        buttonToyama.isHidden = false
     }
 
 
+    //画面遷移
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let read: readViewController = (segue.destination as? readViewController)!
+        read.gate = gate
+    }
 }
 
---
+++ nyukoukanri2021_1/readViewController.swift
@@ -0,0 +1,121 @@
+import UIKit
+import WebKit
+import CoreNFC
+
+class readViewController: UIViewController, NFCTagReaderSessionDelegate, WKNavigationDelegate{
+
+    //初期化
+    var session: NFCTagReaderSession?
+    var gate: String = "str"
+    var idmString: String = "str"
+    var reading: Bool = false
+    var i: Int = 1
+    var sentURL: String = "str"{
+        didSet{
+            openURL(urlString: sentURL)
+        }
+    }
+
+    @IBOutlet weak var reLoad: UIButton!
+    @IBOutlet weak var webView: WKWebView!
+    
+    
+    
+    //関数
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        readNFC()
+    }
+    
+    @IBAction func reLoad(_ sender: Any) {
+        readNFC()
+    }
+    
+    func openURL(urlString: String){
+        let URL = URL(string: urlString)
+        let request = NSURLRequest(url: URL!)
+        webView.load(request as URLRequest)
+        webView.navigationDelegate = self
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!){
+        readNFC()
+    }
+    
+
+    func readNFC(){
+        self.session = NFCTagReaderSession(pollingOption: .iso18092, delegate: self)
+        self.session?.begin()
+        reading = true
+    }
+    
+    
+    
+    
+    func tagReaderSessionDidBecomeActive(_ session: NFCTagReaderSession) {
+        print("読み取りを開始したツノ！")
+    }
+    
+    func tagReaderSession(_ session: NFCTagReaderSession, didInvalidateWithError error: Error) {
+        if let readerError = error as? NFCReaderError{
+            if(readerError.code != .readerSessionInvalidationErrorFirstNDEFTagRead)&&(readerError.code != .readerSessionInvalidationErrorUserCanceled){
+                let alertMessage = UIAlertController(
+                    title: "エラーが発生したツノ",
+                    message: error.localizedDescription,
+                    preferredStyle: .alert
+                )
+                alertMessage.addAction(UIAlertAction(title: "わかったツノ...", style: .default, handler: nil))
+                DispatchQueue.main.async {
+                    self.present(alertMessage, animated: true, completion: nil)
+                }
+            }
+        }
+        self.session = nil
+    }
+    
+    func tagReaderSession(_ session: NFCTagReaderSession, didDetect tags: [NFCTag]) {
+        print("カードが見つかったツノ！")
+        
+        if(tags.count > 1){
+            let retryInterval = DispatchTimeInterval.milliseconds(200)
+            session.alertMessage = "複数枚のカードが見つかったツノ。1枚で試してほしいツノ！"
+            DispatchQueue.global().asyncAfter(deadline: .now() + retryInterval, execute: {
+                session.restartPolling()
+            })
+            return
+        }
+        
+        let tag = tags.first!
+        
+        session.connect(to: tag){(error) in
+            if nil != error{
+                session.invalidate(errorMessage: "接続エラーツノ。もう一度試してほしいツノ！")
+                return
+            }
+            
+            guard case .feliCa(let feliCaTag) = tag else{
+                let retryInterval = DispatchTimeInterval.milliseconds(200)
+                session.alertMessage = "このカードには対応してないツノ..."
+                DispatchQueue.global().asyncAfter(deadline: .now() + retryInterval, execute: {
+                    session.restartPolling()
+                })
+                return
+            }
+            
+            let idmString0: String = feliCaTag.currentIDm.map{String(format: "%.2hhx", $0)}.joined()
+            self.idmString = idmString0.uppercased()
+            print(self.idmString)
+            
+            session.alertMessage = self.idmString
+            session.invalidate()
+            self.reading = false
+            
+            Thread.sleep(forTimeInterval: 1.5)
+            self.sentURL = "https://script.google.com/a/wasedasai.net/macros/s/AKfycbzdl8gVXhd2Dkqy1B6-rTGKD_ewKWpS2FimTIkZiOA2bVf_IQo/exec?idm=" + self.idmString + "&&gate=" + self.gate
+
+        }
+    }
+    
+
+
+}
